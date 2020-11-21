74e0623967a530cec30ebb91bb2367d63dda3cf3
George Kolesnikov <gkolesnikov88@gmail.com>
2020-11-21 17:29:59 +0300

    Add goods enumerator. Change goods id from object id to number.

--- models/Goods.js
+++ models/Goods.js
@@ -1,6 +1,7 @@
 const {Schema, model, Types} = require('mongoose');
 
 const schema = new Schema({
+  _id: {type: Number, required: true},
   name: {type: String, required: true},
   purchasePrice: {type: Number, required: true},
   sellingPrice: {type: Number, required: true},
